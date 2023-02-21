149ec5c0a0ee704eca47ad4dd8ee6a8d5d352ddb
SuperMegacy <pilusaemanuel@gmail.com>
2023-02-20 20:59:34 -0800

    0x00 c hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Emtry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
