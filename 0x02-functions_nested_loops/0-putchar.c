1243dd4616d1ce9d2982b673a48d7c90fca4d936
SuperMegacy <pilusaemanuel@gmail.com>
2023-02-25 07:58:30 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * main - putchar
+ * main - print _putchar
  *
- * Return: Always 0 (success)
+ * Return: 0
  */
+
 int main(void)
 {
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -15,5 +17,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
+
