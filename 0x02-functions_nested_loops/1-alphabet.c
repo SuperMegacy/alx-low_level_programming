932be18494b80dd9c85aa815a51f5682d04c6c33
SuperMegacy <pilusaemanuel@gmail.com>
2023-02-25 08:30:05 -0800

    alphabets'

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - alphabet game
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 96; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	
+	_putchar('\n');
+	return (0);
+}
