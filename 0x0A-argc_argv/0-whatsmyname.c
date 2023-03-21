9038a02d2b5b3bd81470f576f8af993bad45a8f3
SuperMegacy <pilusaemanuel@gmail.com>
2023-03-21 16:25:34 +0200

    my name is

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,13 +9,9 @@
  * Return: Always 0 (success);
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__ ((unused)), char *argv[])
 {
-	int i;
 
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s \n", argv[i]);
-	}
+	printf("%s \n", *argv);
 	return (0);
 }
