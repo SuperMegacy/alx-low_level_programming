9de3882d2b122697de991124bd346955813ea0f4
SuperMegacy <pilusaemanuel@gmail.com>
2023-04-05 16:20:42 +0200

    function taht allocates memory for an array, using malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: array elements
+ * @size: size of array
+ *
+ * Return: none
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *c = malloc(nmemb * size);
+	unsigned int i = 0;
+
+	if (!c)
+		return (NULL);
+
+	for (i = 0; i < nmeb * size; i++)
+		c[i] = 0;
+
+	return (c);
+}
